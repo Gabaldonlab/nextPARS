a430a7051f3472ea63dc4ea56b87ce1a80c417f1
jwillis <jwill123@ufl.edu>
2017-04-05 12:04:53 +0200

    Include python package installation commands

--- packages_for_nextPARS.sh
+++ packages_for_nextPARS.sh
@@ -1,9 +1,9 @@
 ## To install the required python packages
 
+pip install --user argparse biopython datetime gzip numpy pysam termcolor
 
 
 
-
-# Add bin to PYTHONPATH for modules made here
+# Add bin to PYTHONPATH for modules made here (genome_annotations.py)
 cwd=`pwd`
 export PYTHONPATH="$cwd/bin:$PYTHONPATH"
