d86452501cb1e450943d90cd99ec9e21f10a7a82
jwillis <jwill123@ufl.edu>
2017-07-12 16:33:39 +0200

    Update packages_for_nextPARS.sh

--- packages_for_nextPARS.sh
+++ packages_for_nextPARS.sh
@@ -1,3 +1,9 @@
+
+## Install pip for python packages
+sudo apt-get install python-pip
+
+
+
 ## To install the required python packages
 
 pip install --user argparse numpy biopython datetime pysam termcolor
