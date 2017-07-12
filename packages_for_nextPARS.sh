c8bc09cba83c2d16c2597aaf0c9c32b334e37917
jwillis <jwill123@ufl.edu>
2017-07-12 16:49:09 +0200

    Update packages_for_nextPARS.sh

--- packages_for_nextPARS.sh
+++ packages_for_nextPARS.sh
@@ -1,15 +1,13 @@
 
-## Install pip for python packages
-sudo apt-get install python-pip
-
-
-
-## To install the required python packages
-
-pip install --user argparse numpy biopython datetime pysam termcolor
-
-
-
 # Add bin to PYTHONPATH for modules made here (genome_annotations.py)
 cwd=`pwd`
 export PYTHONPATH="$cwd/bin:$PYTHONPATH"
+
+
+## Install pip for python packages
+sudo apt-get install -y python-pip
+
+
+## To install the required python packages
+pip install --user argparse numpy biopython datetime pysam termcolor
+
