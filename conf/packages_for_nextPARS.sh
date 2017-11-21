798f7deef6e39863b12f7658a7e9ff7fd60f465f
jwillis <jwill123@ufl.edu>
2017-11-21 14:50:33 +0100

    Update package installation file

--- conf/packages_for_nextPARS.sh
+++ conf/packages_for_nextPARS.sh
@@ -7,8 +7,9 @@ export PYTHONPATH="$cwd/bin:$PYTHONPATH"
 ## Install pip for python packages
 sudo apt-get update
 sudo apt-get -y install python-pip
+sudo apt-get -y install default-jre
 
 
 ## To install the required python packages
-pip install --user argparse numpy biopython datetime pysam termcolor pandas keras tensorflow dask h5py default-jre
+pip install --user argparse numpy biopython datetime pysam termcolor pandas keras tensorflow dask h5py
 
